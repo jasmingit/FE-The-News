8b00cb2208777f1ffb722a979cb0593f20b6e211
Jasmin Bannister <jasminbannister@Jasmins-MacBook-Pro.local>
2022-08-22 10:06:19 +0100

    react app created

---
+++ the-news/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello this is the news
+      </header>
+    </div>
+  );
+}
+
+export default App;
