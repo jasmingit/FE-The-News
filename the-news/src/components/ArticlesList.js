4ab3840acb2f577f2f897f26cf5ce9567c7fd00c
Jasmin Bannister <jasminbannister@Jasmins-MacBook-Pro.local>
2022-08-24 15:49:34 +0100

    added links to home

--- the-news/src/components/ArticlesList.js
+++ the-news/src/components/ArticlesList.js
@@ -1,5 +1,7 @@
 import {fetchArticles} from "./API/Articles";
 import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import ArticleById from "./Routes/ArticleById";
 
 export default function ArticleList() {
 
@@ -16,7 +18,7 @@ export default function ArticleList() {
             {articles.map((article) => {
                 return (
                     <article className="article-card" key={article.article_id}>
-                        <h2>{article.title}</h2>
+                        <Link onClick={<ArticleById article_id={article.article_id}/>} className="article-head-link" style={{color: 'black'}}to={`/articles/${article.article_id}`}><h2>{article.title}</h2></Link>
                         <h4 className="topic-name">{article.topic}</h4>
                         <p className="author-name">@{article.author}</p>
                         
