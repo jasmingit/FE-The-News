387d43cee5a986622ecea73abd7ae6dbf8653d30
Jasmin Bannister <jasminbannister@Jasmins-MacBook-Pro.local>
2022-08-25 14:47:55 +0100

    added into useEffect

--- the-news/src/components/Comments.js
+++ the-news/src/components/Comments.js
@@ -9,13 +9,12 @@ export default function Comments(article_id) {
         fetchCommentsById(article_id).then((comments) => {
             setComments(comments)
         })
-    })
+    }, [article_id])
     
     return (
         <div className="comments-section">
             {comments.map((comment) => {
                 return(
-                    
                     <section className="comment-card" key={comment.comment_id}>
                         <h5 className="author-name-comment">@{comment.author}</h5>
                         <p className="comment-body">{comment.body}</p>
